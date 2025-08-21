7663a4d373a9e184278215459aca9d168c79a87a
Pr82 <parsa.rohani82@gmail.com>
2025-08-21 18:20:08 +0330

    finished the search bar functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,31 @@
 $(document).ready(function () {
-    $("#search").change(function (e) { 
-        e.preventDefault();
-        
-    });
+  // normalizing the searchbar text
+  const norm = (s) =>
+    (s || "")
+      .toString()
+      .normalize("NFD")
+      .replace(/\p{Diacritic}/gu, "") // remove accents if any
+      .toLowerCase()
+      .trim();
+
+  $("#search").on("input", function () {
+    let query = norm($(this).val());
+
+    // show all items if the search bar is empty
+    if (!query) {
+      $(".item").show();
+      return;
+    }
+
+    $(".item").each(function() {
+        let text = norm($(this).find("h3").text());
+        console.log(text);
+        if (text.includes(query)) {
+            $(this).show();
+        }
+        else {
+            $(this).hide();
+        }
+    })
+  });
 });
