48f7a867da0a21fa845c5e2f21f38c54c3443700
Pr82 <parsa.rohani82@gmail.com>
2025-09-11 11:28:58 +0330

    removed the index.html additional tags, created a function in main.js to fetch the data from menu.json, updated menu.json

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,44 @@
 $(document).ready(function () {
+  // get the data from menu.json
+  $.getJSON("src/menu.json", function (data) {
+    let menuContainer = $(".menu");
+
+    data.categories.forEach((cat) => {
+      // Divider
+      let divider = `
+                <div class="divider" id="${cat.id}">
+                    <hr>
+                    <span>${cat.title}|</span>
+                    <img src="${cat.icon}" alt="${cat.title}">
+                    <hr>
+                </div>
+            `;
+      menuContainer.append(divider);
+
+      // Items
+      let itemsDiv = $('<div class="items"></div>');
+      cat.items.forEach((item) => {
+        let itemHtml = `
+                    <div class="item">
+                        <img src="${item.img}" alt="${item.name}">
+                        <div class="desc">
+                            <h3>${item.name}</h3>
+                            ${item.desc ? `<p>${item.desc}</p>` : ""}
+                            ${
+                              item.price
+                                ? `<p><b>${item.price}</b> تومان</p>`
+                                : ""
+                            }
+                        </div>
+                    </div>
+                `;
+        itemsDiv.append(itemHtml);
+      });
+
+      menuContainer.append(itemsDiv);
+    });
+  });
+
   // normalizing the searchbar text
   const norm = (s) =>
     (s || "")
@@ -17,28 +57,27 @@ $(document).ready(function () {
       return;
     }
 
-    $(".item").each(function() {
-        let text = norm($(this).find("h3").text());
-        console.log(text);
-        if (text.includes(query)) {
-            $(this).show();
-        }
-        else {
-            $(this).hide();
-        }
-    })
+    $(".item").each(function () {
+      let text = norm($(this).find("h3").text());
+      console.log(text);
+      if (text.includes(query)) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
   });
   $(".cat").on("click", function (e) {
-    e.preventDefault(); 
+    e.preventDefault();
 
     const targetId = $(this).attr("href");
     const target = $(targetId);
 
-    if(target.length) {
-        $("html, body").animate(
-            { scrollTop: target.offset().top + 20}, //adjust -20 for padding
-            700 //scroll duration in ms
-        );
+    if (target.length) {
+      $("html, body").animate(
+        { scrollTop: target.offset().top + 20 }, //adjust -20 for padding
+        700 //scroll duration in ms
+      );
     }
   });
 });
